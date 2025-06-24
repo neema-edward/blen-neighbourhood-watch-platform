f7a2e91b0a1092964c6413818db9fa3ec98c604d
Neema <neema.edward@student.moringaschool.com>
2025-06-24 15:31:39 +0300

    Add Appearance model with core attributes and foreign keys

---
+++ server/models/appearance.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Appearance {self.id}>'
