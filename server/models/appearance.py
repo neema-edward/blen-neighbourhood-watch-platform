3cfe5d6dc4e501aa60deb719096e7a18cbb4288f
Neema <neema.edward@student.moringaschool.com>
2025-06-24 15:33:35 +0300

    Add initialization and rating validation to Appearance model

--- server/models/appearance.py
+++ server/models/appearance.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy.orm import validates
 
 class Appearance(db.Model):
     __tablename__ = 'appearances'
@@ -8,5 +9,16 @@ class Appearance(db.Model):
     guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
     episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
     
+    def __init__(self, rating, guest_id, episode_id):
+        self.rating = rating
+        self.guest_id = guest_id
+        self.episode_id = episode_id
+    
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if not (1 <= rating <= 5):
+            raise ValueError('Rating must be between 1 and 5')
+        return rating
+
     def __repr__(self):
         return f'<Appearance {self.id}>'
