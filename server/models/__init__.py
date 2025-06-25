280a17c8eade11826b1263b5c245f05b6ffa3da3
Neema <neema.edward@student.moringaschool.com>
2025-06-25 08:19:35 +0300

    updates models init file

---
+++ server/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import User
+from .report import Report
+from .patrol import Patrol
+from .user_patrol import UserPatrol
+from .community_post import CommunityPost
+from .security_dog import SecurityDog
+
+__all__ = ['User', 'Report', 'Patrol', 'UserPatrol', 'CommunityPost', 'SecurityDog']
